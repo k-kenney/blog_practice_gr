516ca832bfeeadda0f3e60c09555431e95660141
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-05-04 12:57:43 +0200

    Initial commit | Created index and show controllers for the blog post model

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # get 'blog_posts/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
+
+  # Defines the root path route ("/")
+  root "blog_posts#index"
+end
