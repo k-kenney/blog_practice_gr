1a0da9c16cbdcf1b7909a09cc791ba4a0d4e0a03
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-05-14 09:53:27 +0200

    Created 'new' action to blog_practice app

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # get 'blog_posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
-
+  get '/blog_posts/:id', to: 'blog_posts#new', as: :new_blog_post
+  get '/blog_posts/:id', to: 'blog_posts#show', as: :blog_post
+  post '/blog_posts', to: 'blog_posts#create', as: :blog_posts
+  
   # Defines the root path route ("/")
-  root "blog_posts#index"
+  root 'blog_posts#index'
 end
